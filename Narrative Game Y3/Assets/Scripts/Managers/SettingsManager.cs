6b5448ead3c14e2e6c0b68b45ac651397f4cb412
Joshua Mootoo <joshuamoo2+git@gmail.com>
2023-02-27 17:24:26 +0000

    Audio Settings sliders working

--- Narrative Game Y3/Assets/Scripts/Managers/SettingsManager.cs
+++ Narrative Game Y3/Assets/Scripts/Managers/SettingsManager.cs
@@ -8,25 +8,66 @@ using UnityEngine.UI;
 public class VolumeClass
 {
     public Slider slider;
-    public float volume;
     public string playerPrefString;
 }
 
 public class SettingsManager : MonoBehaviour
 {
-    AudioMixer audioMixer;
+    public AudioMixer audioMixer;
 
-    public VolumeClass MasterVolume, MusicVolume, SoundVolume, DialogueVolume;
+    public VolumeClass MasterVolume, MusicVolume, AmbientVolume, DialogueVolume;
 
     private void Start()
     {
-        if (PlayerPrefs.HasKey(MasterVolume.playerPrefString) && MasterVolume.volume != PlayerPrefs.GetFloat(MasterVolume.playerPrefString))
-            MasterVolume.volume = PlayerPrefs.GetFloat(MasterVolume.playerPrefString);
-        if (PlayerPrefs.HasKey(MusicVolume.playerPrefString) && MusicVolume.volume != PlayerPrefs.GetFloat(MusicVolume.playerPrefString))
-            MusicVolume.volume = PlayerPrefs.GetFloat(MusicVolume.playerPrefString);
-        if (PlayerPrefs.HasKey(SoundVolume.playerPrefString) && SoundVolume.volume != PlayerPrefs.GetFloat(SoundVolume.playerPrefString))
-            SoundVolume.volume = PlayerPrefs.GetFloat(SoundVolume.playerPrefString);
-        if (PlayerPrefs.HasKey(DialogueVolume.playerPrefString) && DialogueVolume.volume != PlayerPrefs.GetFloat(DialogueVolume.playerPrefString))
-            DialogueVolume.volume = PlayerPrefs.GetFloat(DialogueVolume.playerPrefString);
+        SoundSettingsStart();
+    }
+
+    private void SoundSettingsStart()
+    {
+        if (PlayerPrefs.HasKey(MasterVolume.playerPrefString) && MasterVolume.slider.value != PlayerPrefs.GetFloat(MasterVolume.playerPrefString))
+        {
+            MasterVolume.slider.value = PlayerPrefs.GetFloat(MasterVolume.playerPrefString);
+        }
+        if (PlayerPrefs.HasKey(MusicVolume.playerPrefString) && MusicVolume.slider.value != PlayerPrefs.GetFloat(MusicVolume.playerPrefString))
+        {
+            MusicVolume.slider.value = PlayerPrefs.GetFloat(MusicVolume.playerPrefString);
+        }
+        if (PlayerPrefs.HasKey(AmbientVolume.playerPrefString) && AmbientVolume.slider.value != PlayerPrefs.GetFloat(AmbientVolume.playerPrefString))
+        {
+            AmbientVolume.slider.value = PlayerPrefs.GetFloat(AmbientVolume.playerPrefString);
+        }
+        if (PlayerPrefs.HasKey(DialogueVolume.playerPrefString) && DialogueVolume.slider.value != PlayerPrefs.GetFloat(DialogueVolume.playerPrefString))
+        {
+            DialogueVolume.slider.value = PlayerPrefs.GetFloat(DialogueVolume.playerPrefString);
+        }
+    }
+
+
+    public void SetMasterVolume(float sliderValue)
+    {
+        audioMixer.SetFloat("MasterAM", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat(MasterVolume.playerPrefString, sliderValue);
+        Debug.Log(PlayerPrefs.GetFloat(MasterVolume.playerPrefString));
+    }
+
+    public void SetMusicVolume(float sliderValue)
+    {
+        audioMixer.SetFloat("MusicAM", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat(MusicVolume.playerPrefString, sliderValue);
+        Debug.Log(PlayerPrefs.GetFloat(MusicVolume.playerPrefString));
+    }
+
+    public void SetAmbientVolume(float sliderValue)
+    {
+        audioMixer.SetFloat("AmbientAM", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat(AmbientVolume.playerPrefString, sliderValue);
+        Debug.Log(PlayerPrefs.GetFloat(AmbientVolume.playerPrefString));
+    }
+
+    public void SetDialogueVolume(float sliderValue)
+    {
+        audioMixer.SetFloat("DialogueAM", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat(DialogueVolume.playerPrefString, sliderValue);
+        Debug.Log(PlayerPrefs.GetFloat(DialogueVolume.playerPrefString));
     }
 }
